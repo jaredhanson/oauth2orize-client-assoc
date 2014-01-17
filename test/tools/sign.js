25dbd8f85b30c949db340494157a6e00fab460a9
Jared Hanson <jaredhanson@gmail.com>
2014-01-17 14:12:12 -0800

    Add tools and keys for generating test JWTs.

---
+++ test/tools/sign.js
@@ -0,0 +1,16 @@
+var jws = require('jws')
+  , fs = require('fs');
+
+
+var payload = {
+  iss: 'http://www.example.com/',
+  software_id: '1234'
+};
+
+var data = jws.sign({
+  header: { alg: 'rs256' },
+  payload: payload,
+  privateKey: fs.readFileSync('../keys/rsa/private-key.pem')
+});
+
+console.log(data);
