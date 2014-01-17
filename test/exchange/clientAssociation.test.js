304da0475d997af09eb22fc13597cd660f055122
Jared Hanson <jaredhanson@gmail.com>
2014-01-17 12:41:22 -0800

    Begin implementation of exchange.

--- test/exchange/clientAssociation.test.js
+++ test/exchange/clientAssociation.test.js
@@ -5,7 +5,44 @@ var chai = require('chai')
 describe('exchange.clientAssociation', function() {
   
   it('should be named client_assoc', function() {
-    expect(clientAssociation().name).to.equal('client_assoc');
+    expect(clientAssociation(function(){}).name).to.equal('client_assoc');
+  });
+  
+  it('should throw if constructed without a issue callback', function() {
+    expect(function() {
+      clientAssociation();
+    }).to.throw(TypeError, 'clientAssociation exchange requires an issue callback');
+  });
+  
+  
+  describe('issuing an access token to new association', function() {
+    var response, err;
+
+    function issue(client, code, redirectURI, done) {
+      return done(new Error('something is wrong'));
+    }
+
+    before(function(done) {
+      chai.connect.use(clientAssociation(issue))
+        .req(function(req) {
+          req.body = { code: 'abc123', redirect_uri: 'http://example.com/oa/callback' };
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should respond with headers', function() {
+      expect(response.getHeader('Content-Type')).to.equal('application/json');
+      expect(response.getHeader('Cache-Control')).to.equal('no-store');
+      expect(response.getHeader('Pragma')).to.equal('no-cache');
+    });
+    
+    it('should respond with body', function() {
+      expect(response.body).to.equal('{"access_token":"s3cr1t","token_type":"Bearer"}');
+    });
   });
   
 });
